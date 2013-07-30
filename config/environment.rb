dc25fba53a0d61b4620f42d10a143a6025f4124a
SwapnilPatil <swapnil.patil04@gmail.com>
2013-07-30 16:59:52 +0530

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -16,8 +16,8 @@ ActionMailer::Base.smtp_settings = {
   
   :authentication => :plain,
   
-  :user_name => "swapnil.patil04@gmail.com",
+  :user_name => "",
   
-  :password => "nseetap"
+  :password => ""
   
 }
